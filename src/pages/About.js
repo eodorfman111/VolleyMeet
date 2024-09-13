1882e694140aca68207faa7ae58360c22ad998d5
eodorfman111 <66503717+eodorfman111@users.noreply.github.com>
2024-09-13 12:48:50 -0400

    Create About.js

---
+++ src/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div className="about-container">
+      <h1>About Volleymeet</h1>
+      <p>
+        Volleymeet is a platform where volleyball enthusiasts can create and join volleyball events, and was created by Leo Dorfman.
+      </p>
+    </div>
+  );
+};
+
+export default About;

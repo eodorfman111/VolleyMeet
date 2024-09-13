48506df681b2a08896c98c72116251c39dabfd48
eodorfman111 <66503717+eodorfman111@users.noreply.github.com>
2024-09-13 13:27:30 -0400

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,9 +6,10 @@ const Navbar = () => {
   return (
     <nav className="navbar">
       <div className="navbar-container">
-        {}
-        <img src="/static/favicon.png" alt="Volleymeet Logo" className="navbar-favicon" />
-        <h2 className="navbar-logo">Volleymeet</h2>
+        <div className="navbar-logo-container">
+          <img src="/static/favicon.png" alt="Volleymeet Logo" className="navbar-favicon" />
+          <h2 className="navbar-logo">Volleymeet</h2>
+        </div>
         <ul className="navbar-links">
           <li>
             <Link to="/">Home</Link>

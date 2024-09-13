2ad5bf785a7aab52799d743324d885903a687614
eodorfman111 <66503717+eodorfman111@users.noreply.github.com>
2024-09-13 12:47:02 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,15 +2,21 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/HomePage';
 import CreateEventPage from './pages/CreateEventPage';
-import EventPage from './pages/EventPage'; 
+import EventPage from './pages/EventPage';
+import Navbar from './components/Navbar'; 
+import About from './pages/About'; 
+import EventsList from './pages/EventsList'; 
 
 function App() {
   return (
     <Router>
+      <Navbar /> {}
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/events" element={<EventsList />} />
         <Route path="/create-event" element={<CreateEventPage />} />
-        <Route path="/event/:id" element={<EventPage />} /> {}
+        <Route path="/event/:id" element={<EventPage />} />
       </Routes>
     </Router>
   );
